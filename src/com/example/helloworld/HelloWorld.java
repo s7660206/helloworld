e3b3efeb20ee057b2ff1ed023e5ba40452c0816f
s7660206 <s7660206@gmail.com>
2021-09-03 22:07:06 +0900

    Initial commit

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        String greeting;
+        greeting = "helloworld";
+        System.out.println(greeting);
+
+        for (int i =1; i<11; i++){
+            System.out.println(+i);
+        }
+    }
+}
